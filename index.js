7accd9a116746dff9252f938ddd890544aceae92
Luis Romero <luisarnaldo11@gmail.com>
2021-01-11 18:51:21 -0500

    CORS y Express - primer commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+require('dotenv').config();
+var cors = require('cors');
+
+const { dbConnection } = require('./database/config');
+
+// Crear el servidor de express
+const app = express();
+
+// Configurar CORS
+app.use(cors())
+
+// Base de datos
+dbConnection();
+
+// Rutas
+app.get('/', (req, res) => {
+    res.status(400).json({
+        ok:true,
+        msg: 'Hola Mundo'
+    })
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Servidor corriendo en puerto ' + 3000);
+})
