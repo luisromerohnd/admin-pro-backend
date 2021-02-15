bcafb7b5750f8a5f296c2e479a2ec44e849df0d7
Luis Romero <luisarnaldo11@gmail.com>
2021-02-15 15:22:57 -0500

    SPA control

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+const path = require('path');
+
 require('dotenv').config();
 var cors = require('cors');
 
@@ -28,6 +31,11 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/upload', require('./routes/uploads'));
+
+// Lo ultimo 
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+})
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + 3000);
